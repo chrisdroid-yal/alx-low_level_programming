b6ff80347228e498c0ea905f245a9f723f4a4eee
chrisdroid <bobitlmr188221@spu.ac.ke>
2025-02-20 03:53:14 +0300

    Edit wunction in '101-quote.c' to contain the correct code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point of my program
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
