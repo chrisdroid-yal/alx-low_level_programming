b59f963372bff7c911085c563ba9d38fb1478f6a
christian <christian.m@yourapps.co.ke>
2025-02-22 07:04:04 +0300

    Fix some bug in the code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,19 +8,19 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = '0'; i <= '2'; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = '0'; j <= '3'; j++)
+		for (j = 0; j <= (i == 2 ? 3 : 9); j++)
 		{
-			for (k = '0'; k <= '5'; k++)
+			for (k = 0; k <= 5; k++)
 			{
-				for (l = '0'; l <= '9'; l++)
+				for (l = 0; l <= 9; l++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar(i + '0');
+					_putchar(j + '0');
 					_putchar(':');
-					_putchar(k);
-					_putchar(l);
+					_putchar(k + '0');
+					_putchar(l + '0');
 					_putchar('\n');
 				}
 			}
